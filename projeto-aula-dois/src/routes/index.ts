7aac93be4ed6ac9ef372f6446d7a3efef314b2e1
Nathan Carlos <nathan324116@gmail.com>
2021-11-16 21:08:44 -0300

    feat(exemplos-http): adiciona exemplos de requisições

--- projeto-aula-dois/src/routes/index.ts
+++ projeto-aula-dois/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import studentsRoutes from './students.routes';
 import helloWorldRoutes from './hello-world.routes';
 import tasksRoutes from './tasks.routes';
+import productRoutes from './products.routes';
 
 const router = Router();
 
 router.use('/students', studentsRoutes);
 router.use('/hello-world', helloWorldRoutes);
 router.use('/tasks', tasksRoutes);
+router.use('/products', productRoutes);
 
 export default router;
